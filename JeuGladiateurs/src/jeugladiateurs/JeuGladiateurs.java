cf4e4351569c159d8ff1f899f892ddfdec30dfa3
202330169 <202330169@PO-24856>
2024-09-10 10:56:14 -0400

    Affiche pre combat

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -14,16 +14,16 @@ public class JeuGladiateurs {
     // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
     CompteurDeTour tour = new CompteurDeTour();
     AffichageEcran affichage = new AffichageEcran();
-    Personnage Bob = new Personnage("Bob le malchanceux",70, 15, 15, 15);
-    Personnage Igor = new Personnage("Igor l'empaleur",100, 25, 5, 30);
+    Personnage Bob = new Personnage("Bob le malchanceux",15, 15, 70, 15);
+    Personnage Igor = new Personnage("Igor l'empaleur",25, 5, 100, 30);
     
     
     
     // </editor-fold>
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
+    Bob.afficherInfoPerso();
+    Igor.afficherInfoPerso();
+    
     // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
     // TODO : Afficher les infos de chaque personnage
     // TODO : Afficher le message du début du combat
