f67bb71959d205bb9942ae4d342fff3a988d2c88
202330169 <202330169@PO-24856>
2024-09-13 15:29:47 -0400

    Mirmillon

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -1,69 +1,62 @@
 package jeugladiateurs;
 
 import personnages.Personnage;
+import personnages.Mirmillon ;
+import personnages.Rétiare ;
 import combat.CompteurDeTour;
 import combat.AffichageEcran;
 
-public class JeuGladiateurs {
 
+public class JeuGladiateurs {
+    
     public static void main(String[] args) {
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
-    CompteurDeTour tour = new CompteurDeTour();
-    AffichageEcran affichage = new AffichageEcran();
-    Personnage personage1 = new Personnage("Bob le malchanceux",15, 15, 70, 15);
-    Personnage personage2 = new Personnage("Igor l'empaleur",25, 5, 100, 30);
-    
-    
-    
-    // </editor-fold>
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="expanded" desc="Instanciation des objets">
+        CompteurDeTour tour = new CompteurDeTour();
+        AffichageEcran affichage = new AffichageEcran();
+        Mirmillon personage1 = new Mirmillon("Bob le malchanceux", 15, 15, 70, 15);
+        Rétiare personage2 = new Rétiare("Igor l'empaleur", 25, 5, 100, 30);
 
-    
-    affichage.affichageDebutCombat();
-    
-    
-    
-        while (personage1.getPointDeVie()> 0 && personage2.getPointDeVie()>0) {
+        // </editor-fold>
+        affichage.affichageDebutCombat();
+        
+        while (personage1.getPointDeVie() > 0 && personage2.getPointDeVie() > 0) {
             tour.afficheTour();
             personage1.setNewInitiative();
             personage2.setNewInitiative();
-    
-        for (int i = 0; i <= 100; i++) {
-            if (personage1.getInitiative() == i) {
-                personage1.frappePersonnage(personage2);
+            
+            for (int i = 0; i <= 100; i++) {
+                if (personage1.getInitiative() == i) {
+                    personage1.frappePersonnage(personage2);
+                }
+                if (personage2.getInitiative() == i) {
+                    personage2.frappePersonnage(personage1);
+                }
             }
-            if (personage2.getInitiative() == i) {
-                personage2.frappePersonnage(personage1);
-            }
-        }
-    affichage.afficherSeparateurInfosPerso();
-    personage1.afficherInfoPerso();
-    personage2.afficherInfoPerso();
-    tour.augmenteTour();
-    affichage.afficherSeparateurTour();
+            
+            affichage.afficherSeparateurPeso();
+            personage1.afficherInfoPerso();
+            personage2.afficherInfoPerso();
+            tour.augmenteTour();
+            affichage.afficherSeparateurTour();
             
         }
-    affichage.affichageVictoir(personage1, personage2);
-    
-        
-        
-    
-    // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
-    // TODO : Afficher les infos de chaque personnage
-    // TODO : Afficher le message du début du combat
-    // </editor-fold>
+        affichage.affichageVictoir(personage1, personage2);
 
-    // **************************************************************************
-    // **************************************************************************
-    // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
-    // TODO : La boucle contenant les étapes du combat
-    // TODO : Après la boucle, afficher le résultat du combat
-    // </editor-fold>
+        // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
+        // TODO : Afficher les infos de chaque personnage
+        // TODO : Afficher le message du début du combat
+        // </editor-fold>
+        // **************************************************************************
+        // **************************************************************************
+        // **************************************************************************
+        // <editor-fold defaultstate="collapsed" desc="Mécanique de combat">
+        // TODO : La boucle contenant les étapes du combat
+        // TODO : Après la boucle, afficher le résultat du combat
+        // </editor-fold>
     }
-
     
 }
