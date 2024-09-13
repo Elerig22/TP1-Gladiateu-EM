d31480edecdb2dd5194d13b481ee7146997633d1
202330169 <202330169@PO-24856>
2024-09-13 13:52:02 -0400

    Separateur

--- JeuGladiateurs/src/combat/AffichageEcran.java
+++ JeuGladiateurs/src/combat/AffichageEcran.java
@@ -4,24 +4,52 @@ import personnages.Personnage;
 
 public class AffichageEcran {
 
+
+    
+    
+    
+
+    public void affichageDebutCombat(){
+        System.out.println("\n\n");
+        System.out.println(">>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<");
+        System.out.println(">>>>> Que le combat commence !  <<<<<");
+        System.out.println(">>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<");
+        System.out.println("\n\n");
+    }
+    
+    public void afficherSeparateurPeso(){
+        System.out.println("\n");
+        System.out.println("----------------");
+        System.out.println("INFOS PERSONNAGE");
+        System.out.println("----------------");
+        
+    }
+    
+    public void afficherSeparateurTour(){
+        System.out.println("*******************************");
+    }
+    
+    
+    
+    
     // **************************************************************************
     // **************************************************************************
     // **************************************************************************
-    // <editor-fold defaultstate="collapsed" desc="MÃ©thodes d'affichage">
+    // <editor-fold defaultstate="collapsed" desc="Méthodes d'affichage">
     public void afficherDebutCombat() {
-        // TODO : Afficher le message du dÃ©but du combat, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher le message du début du combat, tel que montré dans l'énoncé
     }
     
     public void afficherSeparateurDeTour() {
-        // TODO : Afficher un retour Ã  la ligne, suivi d'une ligne d'astÃ©risques, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher un retour à la ligne, suivi d'une ligne d'astérisques, tel que montré dans l'énoncé
     }
 
     public void afficherSeparateurInfosPerso() {
-        // TODO : Afficher un retour Ã  la ligne, suivi du sÃ©parateur "infos personnages", tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher un retour à la ligne, suivi du séparateur "infos personnages", tel que montré dans l'énoncé
     }
 
     public void afficheVictoire(Personnage personnage1, Personnage personnage2) {
-        // TODO : Afficher le gagnant du combat, tel que montrÃ© dans l'Ã©noncÃ©
+        // TODO : Afficher le gagnant du combat, tel que montré dans l'énoncé
     }
     // </editor-fold>
 }
