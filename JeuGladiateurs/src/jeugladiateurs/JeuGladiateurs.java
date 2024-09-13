6c5dbd3b7ac4f1e70300261f77bc0cd47847a11d
202330169 <202330169@PO-24856>
2024-09-13 14:15:26 -0400

    Combat

--- JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
+++ JeuGladiateurs/src/jeugladiateurs/JeuGladiateurs.java
@@ -21,14 +21,35 @@ public class JeuGladiateurs {
     
     // </editor-fold>
 
-    personage1.afficherInfoPerso();
-    personage2.afficherInfoPerso();
+    
     affichage.affichageDebutCombat();
     
-    tour.afficheTour();
     
     
+        while (personage1.getPointDeVie()> 0 && personage2.getPointDeVie()>0) {
+            tour.afficheTour();
+            personage1.setNewInitiative();
+            personage2.setNewInitiative();
     
+        for (int i = 0; i <= 100; i++) {
+            if (personage1.getInitiative() == i) {
+                personage1.frappePersonnage(personage2);
+            }
+            if (personage2.getInitiative() == i) {
+                personage2.frappePersonnage(personage1);
+            }
+        }
+    affichage.afficherSeparateurInfosPerso();
+    personage1.afficherInfoPerso();
+    personage2.afficherInfoPerso();
+    tour.augmenteTour();
+    affichage.afficherSeparateurTour();
+            
+        }
+    
+    
+        
+        
     
     // <editor-fold defaultstate="collapsed" desc="Affichage pré-combat">
     // TODO : Afficher les infos de chaque personnage
